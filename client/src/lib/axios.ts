3a11e52babba54191035fd671365d1eb91ceaf1b
fitman <fitmanverma@gmail.com>
2025-12-25 19:56:14 +0530

    hello

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000', // Server URL
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000', // Server URL
 });
 
 // Add a request interceptor to add the auth token
